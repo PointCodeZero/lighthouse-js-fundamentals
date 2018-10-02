b397d6559eae399d046b0e572421897b1a14afd3
Lucas Cruz <lucas_ftc@hotmail.com>
2018-10-02 23:56:30 +0000

    arrayIceCream.js

---
+++ arrayIceCream.js
@@ -0,0 +1,15 @@
+var iceCreamFlavours = ["chocolate", "vanilla", "cookkies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+//STEP 1
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+//STEP 2
+console.log(iceCreamFlavours[0]);
+
+//STEP 3
+console.log(iceCreamFlavours[iceCreamFlavours.length-1]);
+
+//STEP 4
+console.log(iceCreamFlavours.length);
